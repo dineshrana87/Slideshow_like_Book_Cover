adc8cc05bdd472fbb1c153846cbca3ebdd7989ce
Dinesh Kumar <dineshrana87@gmail.com>
2021-06-18 16:20:21 +0530

    Add project files.

---
+++ Slideshow.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Slideshow_like_Book_Cover
+{
+    public partial class Slideshow : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
